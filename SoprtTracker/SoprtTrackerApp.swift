de9f9615015c8e99e43889cce6e080e4f066d590
Daniil Garpinich <daniil.garpinich@alza.cz>
2026-02-22 18:15:08 +0100

    Initial Commit

---
+++ SoprtTracker/SoprtTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoprtTrackerApp.swift
+//  SoprtTracker
+//
+//  Created by Daniil Garpinich on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SoprtTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
